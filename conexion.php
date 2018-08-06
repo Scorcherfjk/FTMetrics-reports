32ce9622439355c5877efc91f653a4f165c7aaad
Fjavier De Freitas <32197704+Scorcherfjk@users.noreply.github.com>
2018-08-06 12:05:44 -0500

    funcion de tree view

--- conexion.php
+++ conexion.php
@@ -13,4 +13,24 @@
 		$conn = sqlsrv_connect( $serverName, $connectionInfo);
 	}
 
+// function to create dynamic treeview menus
+function createTreeView($parent, $menu) {
+   $html = "";
+   if (isset($menu['parents'][$parent])) {
+      $html .= "
+      <ol class='tree'>";
+       foreach ($menu['parents'][$parent] as $itemId) {
+          if(!isset($menu['parents'][$itemId])) {
+             $html .= "<li><label class='peque' for='subfolder2'><a href='".$menu['items'][$itemId]['link']."'>".$menu['items'][$itemId]['label']."</a></label><input type='checkbox' name='subfolder2'/></li>";
+          }
+          if(isset($menu['parents'][$itemId])) {
+             $html .= "<li><label for='subfolder3'>".$menu['items'][$itemId]['label']."</label> <input type='checkbox' name='subfolder3'/>";
+             $html .= createTreeView($itemId, $menu);
+             $html .= "</li>";
+          }
+       }
+       $html .= "</ol>";
+   }
+   return $html;
+}
 ?>
